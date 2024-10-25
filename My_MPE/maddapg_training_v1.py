b7fb2400c2ef38ebe01d92f777e7e81debba5717
ronchy <rongqi1949@gmail.com>
2024-10-25 21:35:32 +0800

    完成MADDPG debuging

--- My_MPE/maddapg_training_v1.py
+++ My_MPE/maddapg_training_v1.py
@@ -14,10 +14,11 @@ import os
 import time
 import matplotlib.pyplot as plt
 from pettingzoo.mpe import simple_tag_v3
+from maddpg_agent_v1 import Agent
 
 #  Set deviece
 print("torch.cuda.is_available:", torch.cuda.is_available())
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device: {}".format(device) )
 
 
@@ -33,6 +34,16 @@ def multi_observations2state(multi_obs):
 #-----------------------------------------------------------------------------------
 NUM_EPISODE = 10000
 NUM_STEP = 200
+MEMORY_SIZE = 100000
+BATCH_SIZE = 512
+TARGET_UPDATE_INTERVAL = 200  # 最好是整除NUM_STEP。
+# 学习超参数。
+GAMMA = 0.99
+TAU = 0.01
+LR_ACTOR = 1e-2
+LR_CRITIC = 1e-2
+HIDDEN_DIM = 64
+
 
 # 1 Initialize the agents
 env = simple_tag_v3.parallel_env(render_mode="human", num_good = 1, num_adversaries = 3, num_obstacles = 0, max_cycles = 25, continuous_actions = True)
@@ -42,9 +53,14 @@ NUM_AGENT = env.num_agents
 agent_name_list = env.agents  # agent_name_list: ['adversary_0', 'adversary_1', 'adversary_2', 'agent_0']
 print("agent_name_list:",agent_name_list)
 
+scenario = "simple_tag_v3"
+current_path = os.path.dirname(os.path.realpath(__file__))
+agent_path = current_path + "/models/" + scenario + '/'
+timestamp = time.strftime("%Y%m%d%H%M%S")
+
 #  1.1 Get obs_dim
 obs_dim = []
-for agent_obs in multi_observations.vaules():  # 从多个智能体的观测中一个一个取出，进行处理
+for agent_obs in multi_observations.values():  # 从多个智能体的观测中一个一个取出，进行处理
     obs_dim.append(agent_obs.shape[0])
 state_dim = sum(obs_dim)
 #  1.2 Get action_dim
@@ -55,7 +71,10 @@ for agent_name in agent_name_list:
 
 agents = []
 for agent_i in range(NUM_AGENT):
-    agent = Agent(obs_dim = obs_dim, action_dim = action_dim)  # TODO
+    print(f"Initializing agent {agent_i}...")
+    agent = Agent(memory_size = MEMORY_SIZE, obs_dim = obs_dim[agent_i], state_dim = state_dim, n_agent = NUM_AGENT,
+                  action_dim = action_dim[agent_i],alpha = LR_ACTOR, beta = LR_CRITIC, fc1_dim = HIDDEN_DIM,
+                  fc2_dim = HIDDEN_DIM, gamma = GAMMA, tau = TAU, batch_size = BATCH_SIZE )
     agents.append(agent)
 
 # 2 Main training loop
@@ -64,10 +83,10 @@ for episode_i in range(NUM_EPISODE):
 
     # Receive initial state x
     multi_observations, infos = env.reset()
-    epiode_reward = 0
+    episode_reward = 0
     multi_done = {agent_name: False for agent_name in agent_name_list}  # 每局开始，让每个智能体的完成标志清零。
     for step_i in range(NUM_STEP):
-
+        total_step = episode_i*NUM_STEP + step_i
         # 2.1 Collect action from all agents
         multi_actions = {}
         for agent_i, agent_name in enumerate(agent_name_list):
@@ -90,26 +109,144 @@ for episode_i in range(NUM_EPISODE):
             single_action = multi_actions[agent_name]
             single_reward = multi_rewards[agent_name]
             single_done = multi_done[agent_name]
-            agent_replay_buffer.add_memory(single_obs, single_action, single_reward, \
+            agent.replay_buffer.add_memory(single_obs, single_action, single_reward,
                                            single_next_obs, single_done)
-        # update brain every fixed steps
+        # 2.4 update brain every fixed steps
+        multibatch_obses = []
+        multibatch_next_obses = []
+        multibatch_states = []
+        multibatch_next_states = []
+        multibatch_actions = []
+        multibatch_next_actions = []
+        multibatch_online_actions = []
+        multibatch_rewards = []
+        multibatch_dones = []
+        # 2.4.1 sample a batch of memories
+        current_memory_size = min(MEMORY_SIZE, total_step + 1)
+        if current_memory_size < BATCH_SIZE:
+            batch_idx = range(0, current_memory_size)
+        else:
+            #  generate a uniform randome sample from np.arange(current_memory_size) of size (BATCH_SIZE)
+            batch_idx = np.random.choice(current_memory_size, BATCH_SIZE)
+        for agent_i in range(NUM_AGENT):
+            agent = agents[agent_i]
 
+            batch_obses, batch_next_obses, batch_states, batch_next_states, \
+                batch_actions, batch_rewards, batch_dones = agent.replay_buffer.sample(batch_idx)
+
+            batch_obses_tensor = torch.tensor(batch_obses, dtype=torch.float).to(device)
+            batch_next_obses_tensor = torch.tensor(batch_next_obses, dtype=torch.float).to(device)
+            batch_states_tensor = torch.tensor(batch_states, dtype=torch.float).to(device)
+            batch_next_states_tensor = torch.tensor(batch_next_states, dtype=torch.float).to(device)
+            batch_actions_tensor = torch.tensor(batch_actions, dtype=torch.float).to(device)
+            batch_rewards_tensor = torch.tensor(batch_rewards, dtype=torch.float).to(device)
+            batch_dones_tensor = torch.tensor(batch_dones, dtype=torch.float).to(device)
+
+            # multiple + batch
+            multibatch_obses.append(batch_obses_tensor)
+            multibatch_next_obses.append(batch_next_obses_tensor)
+            multibatch_states.append(batch_states_tensor)
+            multibatch_next_states.append(batch_next_states_tensor)
+            multibatch_actions.append(batch_actions_tensor)
+
+            single_batch_next_actions = agent.target_actor.forward(batch_next_obses_tensor)  #  与up 不同 39：45
+            multibatch_next_actions.append(single_batch_next_actions)
+
+            single_batch_online_actions = agent.actor.forward(batch_obses_tensor) #  与up 不同 39：45
+            multibatch_online_actions.append(single_batch_online_actions)
+
+            multibatch_rewards.append(batch_rewards_tensor)
+            multibatch_dones.append(batch_dones_tensor)
+
+        multibatch_actions_tensor = torch.cat(multibatch_actions, dim=1).to(device)
+        multibatch_next_actions_tensor = torch.cat(multibatch_next_actions, dim=1).to(device)
+        multibatch_online_actions_tensor = torch.cat(multibatch_online_actions, dim=1).to(device)
+        # Update critic and actor Network
+        if (total_step+1) % TARGET_UPDATE_INTERVAL == 0:
+            for agent_i in range(NUM_AGENT):
+                agent = agents[agent_i]
+
+                batch_obses_tensor = multibatch_obses[agent_i]
+                batch_next_obses_tensor = multibatch_next_obses[agent_i]
+                batch_states_tensor = multibatch_states[agent_i]
+                batch_next_states_tensor = multibatch_next_states[agent_i]
+                batch_actions_tensor = multibatch_actions[agent_i]
+                batch_rewards_tensor = multibatch_rewards[agent_i]
+                batch_dones_tensor = multibatch_dones[agent_i]
+
+                # target critic Network
+                critic_target_q = agent.target_critic.forward(batch_next_obses_tensor,
+                                                              multibatch_next_actions_tensor.detach())
+                y = (batch_rewards_tensor + (1 - batch_dones_tensor) * agent.gamma * critic_target_q).flatten()
+                # critic Network
+                critic_q = agent.critic.forward(batch_obses_tensor, multibatch_actions_tensor.detach()).flatten()
+
+                critic_loss = nn.MSELoss(y, critic_q)
+                agent.critic.optimizer.zero_grad()
+                critic_loss.backward()
+                agent.critic.optimizer.step()
+
+
+                #Update actor Network
+                actor_loss = agent.critic.forward(batch_states_tensor, multibatch_online_actions_tensor).flatten() # 这个地方可能会出现问题
+                actor_loss = -torch.mean(actor_loss)
+                agent.actor.optimizer.zero_grad()
+                actor_loss.backward()
+                agent.actor.optimizer.step()
+
+                # Update target critic
+                for target_param, param in zip(agent.target_critic.parameters(), agent.critic.parameters()):
+                    target_param.data.copy_(agent.tau * param.data + (1.0 - agent.tau) * target_param.data)
+
+                # Update target actor
+                for target_param, param in zip(agent.target_actor.parameters(), agent.actor.parameters()):
+                    target_param.data.copy_(agent.tau * param.data + (1.0 - agent.tau) * target_param.data)
 
         multi_observations = multi_next_observations
+        episode_reward += sum(single_reward for single_reward in multi_rewards.values())
+        print(f"episode_reward: {episode_reward}")
 
-# 3 Render the env
+    # 3 Render the env
+    if (episode_i + 1) % 100 == 0:
+        env = simple_tag_v3.parallel_env(render_mode="human",
+                                         num_good = 1,
+                                         num_adversaries = 3,
+                                         num_obstacles = 0,
+                                         max_cycles = 25,
+                                         continuous_actions = True)
+        for test_epi_i in range(10):
+            multi_observations, infos = env.reset()
+            for step_i in range(NUM_STEP):
+                multi_actions = {}
+                for agent_i, agent_name in enumerate(agent_name_list):
+                    agent = agents[agent_i]
+                    single_obs = multi_observations[agent_name]
+                    single_action = agent.get_action(single_obs)  # 通过每个智能体自己的观测，分别得到每个agent的action
+                    multi_actions[agent_name] = single_action
+                    multi_next_observations, multi_rewards, multi_done, multi_truncations, infos = env.step(multi_actions)
+                    multi_observations = multi_next_observations
 
-# 4 Save agents' model
+    # 4 Save agents' model
 
+    if episode_i == 0:
+        hightest_reward = episode_reward
+    if episode_reward > hightest_reward:
+        hightest_reward = episode_reward
+        for agent_i in range(NUM_AGENT):
+            agent = agents[agent_i]
+            flag = os.path.exists(agent_path)
+            if not flag:
+                os.makedirs(agent_path)
+            torch.save(agent.actor.state_dict(), f"{agent_path}" + f"agent_{agent_i}_actor_{scenario}_{timestamp}" )
 
-while env.agents:
-    # this is where you would insert your policy
-    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
-    # print("actions:",actions) #  action: array.size = 5
-    multi_next_observations, rewards, terminations, truncations, infos = env.step(actions)
-    # print("observations:",observations)
-    # print("rewards:",rewards)
-    # print("terminations:",terminations)
-    # print("truncations:",truncations)
-    # print("infos:",infos)
+# while env.agents:
+#     # this is where you would insert your policy
+#     actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+#     # print("actions:",actions) #  action: array.size = 5
+#     multi_next_observations, rewards, terminations, truncations, infos = env.step(actions)
+#     # print("observations:",observations)
+#     # print("rewards:",rewards)
+#     # print("terminations:",terminations)
+#     # print("truncations:",truncations)
+#     # print("infos:",infos)
 env.close()
---
+++ My_MPE/maddpg_agent_v1.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+#
+# @Time : 2024-10-25 15:22:13
+# @Author : Ronchy Lu
+# @Email : rongqi1949@gmail.com
+# @File : maddpg_agent_v1.py
+# @Software: PyCharm
+# @Description: None
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import random
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device: {}".format(device))
+class ReplayBuffer():
+    def __init__(self, capacity, obs_dim, state_dim, action_dim, batch_size):
+        self.capacity = capacity
+        self.obs_cap = np.empty((self.capacity, obs_dim))
+        self.next_obs_cap = np.empty((self.capacity, obs_dim))
+        self.state_cap = np.empty((self.capacity, state_dim))
+        self.next_state_cap = np.empty((self.capacity, state_dim))
+        self.action_cap = np.empty((self.capacity, action_dim))
+        self.reward_cap = np.empty((self.capacity, 1))
+        self.done_cap = np.empty((self.capacity, 1), dtype = bool)
+        self.batch_size = batch_size
+        self.current = 0
+    def add_memory(self, obs, next_obs, state, next_state, action, reward, done):
+        self.obs_cap[self.current] = obs
+        self.next_obs_cap[self.current] = next_obs
+        self.state_cap[self.current] = state
+        self.next_state_cap[self.current] = next_state
+        self.action_cap[self.current] = action
+        self.reward_cap[self.current] = reward
+        self.done_cap[self.current] = done
+
+        self.current = (self.current + 1) % self.capacity #  memory空间放满后，又从第一个开始存放。
+
+    def sample(self, idxed):
+        obs = self.obs_cap[idxed]
+        next_obs = self.next_obs_cap[idxed]
+        state = self.state_cap[idxed]
+        next_state = self.next_state_cap[idxed]
+        action = self.action_cap[idxed]
+        reward = self.reward_cap[idxed]
+        done = self.done_cap[idxed]
+        return obs, next_obs, state, next_state, action, reward, done
+
+
+class Actor(nn.Module):
+    def __init__(self, lr_actor, input_dim, fc1_dim, fc2_dim, action_dim):
+        super(Actor, self).__init__()
+        # self.fc1 = nn.Linear(input_dim, fc1_dim)
+        # self.fc2 = nn.linear(fc1_dim, fc2_dim)
+        # self.pi = nn.Linear(fc2_dim, action_dim)
+
+        # 含有两个隐藏层的模型
+        self.model_actor = nn.Sequential(
+            nn.Linear(input_dim, fc1_dim),
+            nn.ReLU(), # sequential中的ReLU不需要参数
+            nn.Linear(fc1_dim, fc2_dim),
+            nn.ReLU(), # sequential中的ReLU不需要参数
+            nn.Linear(fc2_dim, action_dim)
+        )
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr_actor)
+
+    def forward(self, state):
+        # x = F.relu(self.fc1(state))
+        # x = F.relu(self.fc2(x))
+        pi = self.model_actor(state)
+        mu = torch.softmax(pi, dim = 1)
+        return mu
+    def save_checkpoint(self, checkpoint_file):
+        torch.save(self.state_dict(), checkpoint_file)
+    def load_checkpoint(self, checkpoint_file):
+        self.load_state_dict(torch.load(checkpoint_file))
+
+class Critic(nn.Module):
+    def __init__(self, lr_critic, input_dim, fc1_dim, fc2_dim, num_agent, action_dim):
+        super(Critic, self).__init__()
+        # 含有两个隐藏层的模型
+        self.model_critic = nn.Sequential(
+            nn.Linear(input_dim + num_agent * action_dim, fc1_dim),
+            nn.ReLU(),  # sequential中的ReLU不需要参数
+            nn.Linear(fc1_dim, fc2_dim),
+            nn.ReLU(),  # sequential中的ReLU不需要参数
+            nn.Linear(fc2_dim, action_dim)
+        )
+        self.optimizer = torch.optim.Adam(self.parameters(), lr = lr_critic)
+
+    def forward(self, state, action):
+        x = torch.cat([state, action])
+        q = self.model_critic(x)
+        return q
+    def save_checkpoint(self, checkpoint_file):
+        torch.save(self.state_dict(), checkpoint_file)
+    def load_checkpoint(self, checkpoint_file):
+        self.load_state_dict(torch.load(checkpoint_file))
+
+
+class Agent():
+    def __init__(self, memory_size, obs_dim, state_dim, n_agent, action_dim,
+                 alpha, beta, fc1_dim, fc2_dim, gamma, tau, batch_size):
+        self.gamma = gamma
+        self.tau = tau
+        self.action_dim = action_dim
+
+        self.actor = Actor(lr_actor = alpha, input_dim = obs_dim, fc1_dim = fc1_dim, fc2_dim= fc2_dim, \
+                           action_dim = action_dim)
+
+        self.target_actor = Actor(lr_actor=alpha, input_dim=obs_dim, fc1_dim=fc1_dim, fc2_dim=fc2_dim, \
+                           action_dim=action_dim)
+
+        self.critic = Critic(lr_critic = beta, input_dim = state_dim, fc1_dim = fc1_dim, fc2_dim = fc2_dim, \
+                             num_agent = n_agent, action_dim = action_dim)
+        self.target_critic = Critic(lr_critic=beta, input_dim=state_dim, fc1_dim=fc1_dim, fc2_dim=fc2_dim, \
+                             num_agent=n_agent, action_dim=action_dim)
+        self.replay_buffer = ReplayBuffer(capacity=memory_size, obs_dim= obs_dim, state_dim = state_dim,\
+                                          action_dim = action_dim, batch_size = batch_size)
+    def get_action(self, obs):
+        single_obs = torch.tensor(data = obs, dtype = torch.float).unsqueeze(0).to(device)
+        single_action = self.actor.forward(single_obs)
+        noise = torch.randn(self.action_dim).to(device) * 0.2  #缩放一下噪声
+        single_action = torch.clamp(input = single_action+noise, min = 0.0, max = 1.0)  #限制在0-1之间
+        return single_action.detach().cpu().numpy()[0]
+
+    def save_model(self, filename):
+        self.actor.save_checkpoint(filename)
+        self.target_actor.save_checkpoint(filename)
+        self.critic.save_checkpoint(filename)
+        self.target_critic.save_checkpoint(filename)
+    def load_model(self, filename):
+        self.actor.load_checkpoint(filename)
+        self.target_actor.load_checkpoint(filename)
+        self.critic.load_checkpoint(filename)
+        self.target_critic.load_checkpoint(filename)
+
+
+
