4f6b4e0066265244625d14a2809edb2927cb5b7c
ronchy <rongqi1949@gmail.com>
2024-11-16 18:09:20 +0800

    修改random_step及size_win参数

--- My_MADDPG_Continous/main_parameters.py
+++ My_MADDPG_Continous/main_parameters.py
@@ -5,11 +5,11 @@ def main_parameters():
     parser.add_argument("--env_name", type =str, default = "simple_tag_v3", help = "name of the env",
                         choices=['simple_adversary_v3', 'simple_spread_v3', 'simple_tag_v3'])
     parser.add_argument("--render_mode", type=str, default = "None", help = "None | human | rgb_array")
-    parser.add_argument("--episode_num", type = int, default = 5000)
+    parser.add_argument("--episode_num", type = int, default = 5000) # 5000
     parser.add_argument("--episode_length", type = int, default = 50)
     parser.add_argument('--learn_interval', type=int, default=50,
                         help='steps interval between learning time')
-    parser.add_argument('--random_steps', type=int, default=2e3, help='random steps before the agent start to learn')
+    parser.add_argument('--random_steps', type=int, default=500, help='random steps before the agent start to learn') #  2e3
     parser.add_argument('--tau', type=float, default=0.02, help='soft update parameter')
     parser.add_argument('--gamma', type=float, default=0.7, help='discount factor')
     parser.add_argument('--buffer_capacity', type=int, default=int(1e6), help='capacity of replay buffer')
@@ -19,7 +19,7 @@ def main_parameters():
     # The parameters for the communication network
     # TODO
     parser.add_argument('--visdom', type=bool, default=True, help="Open the visdom")
-    parser.add_argument('--size_win', type=int, default=1000, help="Open the visdom")
+    parser.add_argument('--size_win', type=int, default=200, help="Open the visdom") # 1000
 
 
     args = parser.parse_args()
