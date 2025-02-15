942088348eda76cb6c3d266142e915add24a5ebf
ronchy_lab <rongqi1949@gmail.com>
2025-02-16 00:43:26 +0800

    update train parameters

--- My_MADDPG_Continous/main_train.py
+++ My_MADDPG_Continous/main_train.py
@@ -42,9 +42,10 @@ if __name__ == '__main__':
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     device = torch.device('mps' if hasattr(torch.backends, 'mps') and torch.backends.mps.is_available() 
                             else 'cuda' if torch.cuda.is_available() else 'cpu')
+    device = "cpu"
     print("Using device:",device)
     start_time = time.time() # 记录开始时间
-    # device = "cpu"
+    
     # 模型保存路径
     chkpt_dir='models/maddpg_models/'
     # 定义参数
